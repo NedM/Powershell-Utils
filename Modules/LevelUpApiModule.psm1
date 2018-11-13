da82259500d901567141cdcdb5b167dc789513c1
Ned Murphy <ned@thelevelup.com>
2018-11-13 14:01:55 -0500

    Adding support for receipt scan endpoints
    * Adding localhost to list of known environments

--- Modules/LevelUpApiModule.psm1
+++ Modules/LevelUpApiModule.psm1
@@ -6,6 +6,7 @@
 $Script:pathToConfig = ("{0}\LevelUpConfig.json" -f $PSScriptRoot)
 
 ## Base Uris ##
+$localhostURI = "http://localhost:5001/"
 $productionBaseURI = "https://api.thelevelup.com/"
 $stagingBaseURI = "https://api.staging-levelup.com/"
 $sandboxBaseURI = "https://sandbox.thelevelup.com/"
@@ -37,7 +38,12 @@ $Script:environment = ''
 [int]$Script:version = ''
 $Script:allowed_fulfillment_types = @('in_store', 'pickup', 'delivery')
 
-$environments = @{"production" = $productionBaseURI; "sandbox" = $sandboxBaseURI; "staging" = $stagingBaseURI }
+$environments = @{
+    "localhost" = $localhostURI;
+    "production" = $productionBaseURI;
+    "sandbox" = $sandboxBaseURI;
+    "staging" = $stagingBaseURI
+}
 
 Import-Module $PSScriptRoot\JsonFileOperations.psm1 -force
 
@@ -99,6 +105,10 @@ function Set-LevelUpEnvironment{
         $global:baseURI = $environments[$envName.ToLower()]
     }
 
+    if(!$global:baseURI.EndsWith('/')) {
+        $global:baseURI += '/'
+    }
+
     switch($version) {
         0 { $global:ver = $null }
         13 { $global:ver = $v13 }
@@ -126,6 +136,7 @@ function Load-LevelUpConfigFromFile {
 
     Load-LevelUpConfig($config)
 }
+
 function Load-LevelUpConfig {
     [cmdletbinding()]
     Param(
@@ -311,7 +322,7 @@ function Submit-LevelUpProposedOrder {
 
     $accessToken = "merchant=" + $merchantAccessToken
 
-    $response = Submit-PostRequest $theURI $body $accessToken
+    $response = Submit-PostRequest -uri $theURI -body $body -accessToken $accessToken
     $parsed = $response.Content | ConvertFrom-Json
 
     return $parsed.proposed_order
@@ -829,6 +840,69 @@ function Get-LevelUpLocationsForApp{
     return $parsed.location
 }
 
+## RECEIPT SCANS ##
+# Get Receipt Deposit Location #
+function Get-LevelUpReceiptScanLocation{
+    [cmdletbinding()]
+    Param(
+        [Parameter(Mandatory=$false)]
+        [string]$userAccessToken = $Script:merchantAccessToken
+    )
+
+    $accessToken = "user={0}" -f $userAccessToken
+
+    $theUri = "{0}receipt_scans/image_upload" -f ($global:baseURI+$v15)
+
+    $response = Submit-GetRequest -uri $theURI -accessToken $accessToken
+
+    $parsed = $response.Content | ConvertFrom-Json
+
+    return $parsed.receipt_scan_image_upload
+}
+
+## Upload Receipt ##
+function Send-LevelUpReceiptScan{
+    [cmdletbinding()]
+    Param(
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory=$true)]
+        [int[]]$campaignIds,
+        [Parameter(Mandatory=$true)]
+        [int]$locationId,
+        [Parameter(Mandatory=$true)]
+        [string]$checkId,
+        [Parameter(Mandatory=$true)]
+        [int]$subtotalAmount,
+        [Parameter(Mandatory=$true)]
+        [string]$urlToPhoto,
+        [Parameter(Mandatory=$true)]
+        [string]$userAccessToken
+    )
+
+    $currentDateTime = Get-Date -format "yyyy-MM-ddTHH:mm"
+    $receiptScan = @{
+        'receipt_scan' = @{
+            'campaign_ids' = $campaignIds;  # Campaigns must be a type that can be forwarded and active at location
+            'location_id' = $locationId;  # Location must be running the campaigns specified above
+            'check_identifier' = $checkId;
+            'scan_reason' = 'I am making a test!';
+            'receipt_at' = $currentDateTime;
+            'subtotal_amount' = $subtotalAmount;
+            'image_url' = $urlToPhoto;  # Url to photo on any publicly shared hosting service (e.g. dropbox, google etc.)
+        }
+    }
+
+    $accessToken = "user={0}" -f $userAccessToken
+
+    $theUri = "{0}receipt_scans" -f ($global:baseURI+$v15)
+
+    $body = $receiptScan | ConvertTo-Json
+
+    $response = Submit-PostRequest -uri $theURI -accessToken $accessToken -body $body
+
+    return $response.StatusCode # HTTP status 204 [No content] indicates success
+}
+
 #################################
 # LevelUp Order Ahead API Calls #
 #################################
@@ -864,6 +938,7 @@ function Complete-LevelUpOAOrderById {
     )
     return Complete-LevelUpOAOrder -userToken $userToken -url ("{0}order_ahead/order/{1}/complete" -f ($global:baseURI+$v15), $orderUuid)
 }
+
 function Complete-LevelUpOAOrder {
     [cmdletbinding()]
     param(
@@ -1258,7 +1333,7 @@ function HandleWebException {
     }
     catch {
         # Just output the body as raw data
-        Write-Host $global:responseBody -ForegroundColor Red 
+        Write-Host $global:responseBody -ForegroundColor Red
     }
     break
 }
