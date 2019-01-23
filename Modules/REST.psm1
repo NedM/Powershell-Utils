6c44085445774193fc237892f94569d11b87fd1a
Ned Murphy <Ned@TheLevelUp.com>
2019-01-23 15:23:42 -0500

    Logging tweaks for web request exception handling
    * Adding handling for HttpResponseException

--- Modules/REST.psm1
+++ Modules/REST.psm1
@@ -46,13 +46,16 @@ function Submit-GetRequest{
         [Hashtable]$headers
     )
 
-    Write-Verbose "Calling GET on $uri"
+    Write-Verbose "Calling +[GET]+ on $uri"
 
     try {
-        return Invoke-WebRequest -Method Get -Uri $uri -Headers $headers
+        return Invoke-WebRequest -Method Get -Uri $uri -Headers $headers -ErrorAction:Stop
     }
     catch [System.Net.WebException] {
         HandleWebException($_.Exception)
+    }
+    catch [Microsoft.Powershell.Commands.HttpResponseException] {
+        HandleHttpResponseException($_.Exception)
     }
 }
 
@@ -67,13 +70,16 @@ function Submit-PostRequest{
         [Hashtable]$headers
     )
 
-    Write-Verbose "Calling POST on $uri`nBody:`n$body"
+    Write-Verbose "Calling +[POST]+ on $uri`nBody:`n$body"
 
     try {
-        return Invoke-WebRequest -Method Post -Uri $uri -Body $body -Headers $headers
+        return Invoke-WebRequest -Method Post -Uri $uri -Body $body -Headers $headers -ErrorAction:Stop
     }
     catch [System.Net.WebException] {
         HandleWebException($_.Exception)
+    }
+    catch [Microsoft.Powershell.Commands.HttpResponseException] {
+        HandleHttpResponseException($_.Exception)
     }
 }
 
@@ -87,33 +93,77 @@ function Submit-PutRequest{
         [Parameter(Mandatory=$false)]
         [Hashtable]$headers
     )
-    Write-Verbose "Calling PUT on $uri`nBody:`n$body"
+    Write-Verbose "Calling +[PUT]+ on $uri`nBody:`n$body"
 
     try {
-        return Invoke-WebRequest -Method Put -Uri $uri -Body $body -Headers $headers
+        return Invoke-WebRequest -Method Put -Uri $uri -Body $body -Headers $headers -ErrorAction:Stop
     }
     catch [System.Net.WebException] {
         HandleWebException($_.Exception)
     }
+    catch [Microsoft.Powershell.Commands.HttpResponseException] {
+        HandleHttpResponseException($_.Exception)
+    }
+}
+
+function HandleHttpResponseException {
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [Microsoft.Powershell.Commands.HttpResponseException]$exception
+    )
+    if (!$exception.Response) {
+        Write-Host $exception -ForegroundColor Red
+        break
+    }
+
+    $statusCode = [int]$exception.Response.StatusCode
+    $statusDescription = $exception.Response.ReasonPhrase
+    Write-Host -ForegroundColor:Red "HTTP Error [$statusCode]: $statusDescription"
+
+    $lastError = $Global:Error | Select-Object -Last 1
+    if ($lastError) {
+        $parsed = $lastError.ErrorDetails.Message | ConvertFrom-Json
+        Write-Host "Error message:" -ForegroundColor:DarkGray
+        Write-Host "`t" $parsed.Error.Message -ForegroundColor:DarkGray
+    }
+    break
 }
 
 function HandleWebException([System.Net.WebException]$exception) {
+
+    if(!$exception.Response) {
+        Write-Host $exception -ForegroundColor:Red
+        break
+    }
+
     $statusCode = [int]$exception.Response.StatusCode
     $statusDescription = $exception.Response.StatusDescription
-    Write-Host -ForegroundColor:Red "HTTP Error: $statusCode $statusDescription"
+    Write-Host -ForegroundColor:Red "HTTP Error [$statusCode]: $statusDescription"
 
-    # Get the response body as JSON
-    $responseStream = $exception.Response.GetResponseStream()
-    $reader = New-Object System.IO.StreamReader($responseStream)
-    $global:responseBody = $reader.ReadToEnd()
-    Write-Host "Error message:"
+    $responseStream = $null
     try {
-        $json = $global:responseBody | ConvertFrom-JSON
-        $json | ForEach-Object { Write-Host  "    " $_.error.message }
-    }
-    catch {
-        # Just output the body as raw data
-        Write-Host -ForegroundColor:Red $global:responseBody
+        # Get the response body as JSON
+        $responseStream = $exception.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($responseStream)
+        $global:responseBody = $reader.ReadToEnd()
+
+        if($global:responseBody) {
+            Write-Host "Error message:" -ForegroundColor:DarkGray
+            try {
+                $json = $global:responseBody | ConvertFrom-JSON
+                $json | ForEach-Object { Write-Host  "`t" $_.error.message -ForegroundColor:DarkGray }
+            }
+            catch {
+                # Just output the body as raw data
+                Write-Host $global:responseBody -ForegroundColor:DarkGray
+            }
+        }
+    } finally {
+        if($responseStream) {
+            $responseStream.Close()
+            $responseStream.Dispose()
+        }
     }
     break
 }
